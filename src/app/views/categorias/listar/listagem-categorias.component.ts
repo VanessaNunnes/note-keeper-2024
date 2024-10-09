20561375d3b8f1a874f830fe8596343e66415ecb
VanessaNunnes <vanessafurtadonunes@gmail.com>
2024-10-09 17:16:39 -0300

    Refatorada listagem para usar observable

--- src/app/views/categorias/listar/listagem-categorias.component.ts
+++ src/app/views/categorias/listar/listagem-categorias.component.ts
@@ -3,27 +3,24 @@ import { ListagemCategorias } from '../models/categoria.models';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { NgForOf } from '@angular/common';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CategoriaService } from '../services/categoria.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-listagem-categorias',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatIconModule, MatTooltipModule, NgForOf, RouterLink],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, MatTooltipModule, NgForOf, RouterLink, AsyncPipe, NgIf],
   templateUrl: './listagem-categorias.component.html',
   styleUrl: './listagem-categorias.component.scss'
 })
 export class ListagemCategoriasComponent implements OnInit {
-  categorias: ListagemCategorias[];
+  categorias$?: Observable<ListagemCategorias[]>;
 
-  constructor(private categoriaService: CategoriaService){
-    this.categorias = [];
-  }
+  constructor(private categoriaService: CategoriaService){}
   ngOnInit(): void {
-    this.categoriaService.selecionarTodos().subscribe(res => {
-      this.categorias = res;
-    }) ;
+    this.categorias$ = this.categoriaService.selecionarTodos();
   }
 }
