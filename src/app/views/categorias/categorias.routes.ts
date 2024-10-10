202da7964cf4be33928fd4dbe7dc63cdcc87fc25
VanessaNunnes <vanessafurtadonunes@gmail.com>
2024-10-10 16:14:45 -0300

    Implementada exclusão de categorias

--- src/app/views/categorias/categorias.routes.ts
+++ src/app/views/categorias/categorias.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from "@angular/router";
 import { CadastroCategoriaComponent } from "./cadastrar/cadastro-categoria.component";
 import { EdicaoCategoriaComponent } from "./editar/edicao-categoria.component";
-//import { ExclusaoCategoriaComponent } from "./excluir/exclusao-categoria.component";
+import { ExclusaoCategoriaComponent } from "./excluir/exclusao-categoria.component";
 import { ListagemCategoriasComponent } from "./listar/listagem-categorias.component";
 
 export const categoriasRoutes: Routes = [
@@ -9,5 +9,5 @@ export const categoriasRoutes: Routes = [
   { path: 'listar', component: ListagemCategoriasComponent },
   { path: 'cadastrar', component: CadastroCategoriaComponent },
   { path: 'editar/:id', component: EdicaoCategoriaComponent },
-  //{ path: 'excluir/:id', component: ExclusaoCategoriaComponent },
+  { path: 'excluir/:id', component: ExclusaoCategoriaComponent },
 ]
