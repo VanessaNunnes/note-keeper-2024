b1bd5ce33c8aee87021ea6844dbbf29791ab5e67
VanessaNunnes <vanessafurtadonunes@gmail.com>
2024-10-10 16:33:24 -0300

    Implementada edição de notas

--- src/app/views/notas/notas.routes.ts
+++ src/app/views/notas/notas.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListagemNotasComponent } from './listar/listagem-notas.component';
 import { CadastroNotaComponent } from './cadastrar/cadastro-nota.component';
+import { EdicaoNotaComponent } from './editar/edicao-nota.component';
+//import { ExclusaoNotaComponent } from './excluir/exclusao-nota.component';
 
 export const notasRoutes: Routes = [
   {
@@ -10,4 +12,6 @@ export const notasRoutes: Routes = [
   },
   { path: 'listar', component: ListagemNotasComponent },
   { path: 'cadastrar', component: CadastroNotaComponent },
+  { path: 'editar/:id', component: EdicaoNotaComponent },
+  //{ path: 'excluir/:id', component: ExclusaoNotaComponent },
 ];
